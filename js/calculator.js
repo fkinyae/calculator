6696944b4e56673fdccb95b78a789a1217db0593
Francis Kinyae <fmkinyae@gmail.com>
2021-06-10 09:18:52 +0300

    added add function

---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+let add = function (num1,num2) {
+    return num1 + num2;
+}
+
+let num1 = parseInt(prompt("Enter number 1: "));
+let num2 = parseInt(prompt("Enter number 2: "));
+
+alert(add(num1,num2));
