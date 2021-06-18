c0c87a1cab896f994760a57602bec2614f5eca76
Francis Kinyae <fmkinyae@gmail.com>
2021-06-18 12:07:28 +0300

    add calculator form

--- js/calculator.js
+++
@@ -1,8 +0,0 @@
-let add = function (num1,num2) {
-    return num1 + num2;
-}
-
-let num1 = parseInt(prompt("Enter number 1: "));
-let num2 = parseInt(prompt("Enter number 2: "));
-
-alert(add(num1,num2));
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let number1 = parseInt(prompt("Enter number 1: "));
+let number2 = parseInt(prompt("Enter number 2: "));
+
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
+
+//alert(subtract(num1,num2));
+//alert(add(num1,num2));
+//alert(multiply(num1,num2));
+
